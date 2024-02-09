cf9a3da0215c9d3a38a1f5d43499d573b48faa97
Arkadiusz <stefanski.a00@gmail.com>
2024-02-09 15:15:52 +0100

    make build

---
+++ index.a5267e76.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree"),t=document.querySelectorAll("li"),r=!0,n=!1,a=void 0;try{for(var l,d=t[Symbol.iterator]();!(r=(l=d.next()).done);r=!0){var i=l.value,o=document.createElement("span");i.prepend(o),o.append(o.nextSibling)}}catch(e){n=!0,a=e}finally{try{r||null==d.return||d.return()}finally{if(n)throw a}}e.addEventListener("click",function(e){if("SPAN"===e.target.tagName){var t=e.target.parentNode.querySelector("ul");t&&(t.hidden=!t.hidden)}});//# sourceMappingURL=index.a5267e76.js.map
+
+//# sourceMappingURL=index.a5267e76.js.map
